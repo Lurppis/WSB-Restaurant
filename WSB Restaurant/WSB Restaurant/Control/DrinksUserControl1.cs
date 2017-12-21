3f37c7f9f71714127eb3145247374559c3e3c46f
Lurppis <marcinpilarczyk@me.com>
2017-12-21 13:02:23 +0100

    Save changes for drink action conterol

---
+++ WSB Restaurant/WSB Restaurant/Control/DrinksUserControl1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSB_Restaurant.Control
+{
+    public partial class DrinksUserControl1 : UserControl
+    {
+        public DrinksUserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
